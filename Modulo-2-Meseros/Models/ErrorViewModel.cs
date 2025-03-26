a04cd08cae94910c2e9cbbbbfc96253feb105757
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-03-26 04:28:23 -0600

    Commit

---
+++ Modulo-2-Meseros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Modulo_2_Meseros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
